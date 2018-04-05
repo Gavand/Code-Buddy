539fd2c9efa69f597757c70d70477a857b3d13bc
Gavin Andrews <Gavand@macbook-pro-3.local>
2018-04-05 14:26:59 -0700

    Initial Commit

---
+++ CodeBuddy/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CodeBuddy
+//
+//  Created by Gavin Andrews on 4/5/18.
+//  Copyright © 2018 Gavin Andrews. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CodeBuddyTests/CodeBuddyTests.swift
@@ -0,0 +1,36 @@
+//
+//  CodeBuddyTests.swift
+//  CodeBuddyTests
+//
+//  Created by Gavin Andrews on 4/5/18.
+//  Copyright © 2018 Gavin Andrews. All rights reserved.
+//
+
+import XCTest
+@testable import CodeBuddy
+
+class CodeBuddyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
